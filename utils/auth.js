e389db3053f1fba9d6a4ccb9367afb72ffbf481d
mahdiAsadollahi <mhdiasdollahi@gmail.com>
2025-12-08 03:26:54 +0330

    add auth file

---
+++ utils/auth.js
@@ -0,0 +1,64 @@
+const { hash, compare } = require("bcryptjs");
+const { sign } = require("jsonwebtoken");
+
+const hashPassword = async (password) => {
+  const hashedPassword = await hash(password, 12);
+  return hashedPassword;
+};
+
+const verifyPassword = async (password, hashedPassword) => {
+  const isValid = await compare(password, hashedPassword);
+  return isValid;
+};
+
+const generateAccessToken = (data) => {
+  const token = sign({ ...data }, process.env.AccessTokenSecretKey, {
+    expiresIn: "1h",
+  });
+  return token;
+};
+
+const verifyAccessToken = (token) => {
+  try {
+    const tokenPayload = verify(token, process.env.AccessTokenSecretKey);
+    return tokenPayload;
+  } catch (err) {
+    console.log("Verify Access Token Error ->", err);
+    return false;
+  }
+};
+
+const generateRefreshToken = (data) => {
+  const token = sign({ ...data }, process.env.RefreshTokenSecretKey, {
+    expiresIn: "7d",
+  });
+  return token;
+};
+
+const validateEmail = (email) => {
+  const pattern =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return pattern.test(email);
+};
+
+const validatePhone = (phone) => {
+  const pattern = /^(\+98|0)?9\d{9}$/;
+  return pattern.test(phone.replace(/\s+/g, ""));
+};
+
+const valiadtePassword = (password) => {
+  const pattern =
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/g;
+  return pattern.test(password);
+};
+
+export {
+  hashPassword,
+  verifyPassword,
+  generateAccessToken,
+  verifyAccessToken,
+  generateRefreshToken,
+  valiadteEmail,
+  valiadtePhone,
+  valiadtePassword,
+};

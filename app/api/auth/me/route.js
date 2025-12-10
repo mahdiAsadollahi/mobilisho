c4fce28f5555b7cf5be5f4617f33883844d42ba6
mahdiAsadollahi <mhdiasdollahi@gmail.com>
2025-12-10 11:50:57 +0330

    create get me api

---
+++ app/api/auth/me/route.js
@@ -0,0 +1,33 @@
+import connectToDB from "@/configs/db";
+import { verifyAccessToken } from "@/utils/auth";
+import { cookies } from "next/headers";
+import UserModel from "@/models/User";
+
+export async function GET(req) {
+  connectToDB();
+  const token = cookies().get("token");
+  let user = null;
+
+  if (token) {
+    const tokenPayload = verifyAccessToken(token.value);
+
+    if (tokenPayload) {
+      user = await UserModel.findOne(
+        { phone: tokenPayload.phone },
+        "username phone role isBan"
+      );
+    }
+
+    return Response.json(user);
+  } else {
+    return Response.json(
+      {
+        data: null,
+        message: "دسترسی غیرمجاز",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+}

039ffdac8988382a7b4856f6582d4e62fdc63711
mahdiAsadollahi <mhdiasdollahi@gmail.com>
2026-01-27 18:55:29 +0330

    create api/user -> delete

--- app/api/users/[id]/delete/route.js
+++ app/api/users/[id]/delete/route.js
@@ -1,5 +1,40 @@
+import connectToDB from "@/configs/db";
+import UserModel from "@/models/User";
+
 export async function DELETE(req, { params }) {
   try {
+    await connectToDB();
+
+    const { id } = await params;
+
+    const user = await UserModel.findById(id);
+
+    if (!user) {
+      return Response.json({ message: "کاربر یافت نشد" }, { status: 404 });
+    }
+
+    const deletedUser = await UserModel.findOneAndDelete({ _id: id }, {});
+
+    if (!deletedUser) {
+      return Response.json({ message: "حذف کاربر انجام نشد" }, { status: 500 });
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "کاربر با موفقیت حذف شد",
+        data: {
+          id: deletedUser._id,
+          username: deletedUser.username,
+          phone: deletedUser.phone,
+          role: deletedUser.role,
+          deletedAt: new Date(),
+        },
+      },
+      {
+        status: 200,
+      }
+    );
   } catch (err) {
     return Response.json(
       { message: "خطا در حذف کاربر !", error: err.message },

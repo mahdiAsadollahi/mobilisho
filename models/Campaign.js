f580e4e6aa97a779d5c2117c0a6bcb53e0c24a55
mahdiAsadollahi <mhdiasdollahi@gmail.com>
2026-01-30 21:55:28 +0330

    create Campaign model

---
+++ models/Campaign.js
@@ -0,0 +1,57 @@
+const { default: mongoose } = require("mongoose");
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  scope: {
+    type: String,
+    enum: ["all_products", "specific_category", "specific_products"],
+    required: true,
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+  discount_type: {
+    type: String,
+    enum: ["percentage", "fixed"],
+    required: true,
+  },
+  discount_value: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  status: {
+    type: String,
+    enum: ["active", "inactive", "ended"],
+    default: "active",
+  },
+  start_date: {
+    type: Date,
+    required: true,
+  },
+  end_date: {
+    type: Date,
+    required: true,
+  },
+  categories: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    },
+  ],
+  products: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+    },
+  ],
+});
+
+const model = mongoose.models.Campaign || mongoose.model("Campaign", schema);
+
+module.exports = model;

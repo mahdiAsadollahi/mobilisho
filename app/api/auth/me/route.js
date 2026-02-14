e169787a8dc6faf7026797237ac927e658f7ad50
mahdiAsadollahi <mhdiasdollahi@gmail.com>
2026-02-14 11:11:49 +0330

    refactor get me api

--- app/api/auth/me/route.js
+++ app/api/auth/me/route.js
@@ -1,33 +1,63 @@
+// app/api/user/me/route.js
 import connectToDB from "@/configs/db";
 import { verifyAccessToken } from "@/utils/auth";
 import { cookies } from "next/headers";
 import UserModel from "@/models/User";
 
 export async function GET(req) {
-  connectToDB();
-  const token = cookies().get("token");
-  let user = null;
+  try {
+    await connectToDB();
 
-  if (token) {
+    const token = cookies().get("token");
+
+    if (!token) {
+      return Response.json(
+        {
+          data: null,
+          message: "دسترسی غیرمجاز",
+        },
+        { status: 401 }
+      );
+    }
+
     const tokenPayload = verifyAccessToken(token.value);
 
-    if (tokenPayload) {
-      user = await UserModel.findOne(
-        { phone: tokenPayload.phone },
-        "_id username phone role isBan"
+    if (!tokenPayload) {
+      return Response.json(
+        {
+          data: null,
+          message: "توکن نامعتبر است",
+        },
+        { status: 401 }
+      );
+    }
+
+    // پیدا کردن کاربر با شماره تلفن از توکن
+    const user = await UserModel.findOne(
+      { phone: tokenPayload.phone },
+      "_id username phone role isBan"
+    );
+
+    if (!user) {
+      return Response.json(
+        {
+          data: null,
+          message: "کاربر یافت نشد",
+        },
+        { status: 404 }
       );
     }
 
     return Response.json(user);
-  } else {
+  } catch (err) {
+    console.log("Error in GET user/me:", err.message);
+
     return Response.json(
       {
         data: null,
-        message: "دسترسی غیرمجاز",
+        message: "خطا در دریافت اطلاعات کاربر",
       },
-      {
-        status: 401,
-      }
+      { status: 500 }
     );
   }
 }
